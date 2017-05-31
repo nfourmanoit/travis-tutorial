4aac5ab7ec283dcc525df58f883e9c46e8edf793
Nicolas Fourmanoit <fourmanoit@cppm.in2p3.fr>
2017-05-31 12:21:53 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,5 +1,11 @@
+import numpy as np
 def test_addition():
     assert 1 + 1 == 2
     
 def test_division():
     assert 3./2 == 1.5
+
+def  test_numpy_partition(): 
+     input_arr = np.aray()[3, 2, -1, 100 ]
+     partitioned = np.partition(input_arr, 1)
+     assert partitioned[1] == 2
