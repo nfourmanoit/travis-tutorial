1b92386241a14c1b5afb4c6deea7c3c56f66ccb8
Nicolas Fourmanoit <fourmanoit@cppm.in2p3.fr>
2017-05-31 14:51:20 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,11 +1,22 @@
 import numpy as np
+
+
 def test_addition():
     assert 1 + 1 == 2
-    
+
+
 def test_division():
     assert 3./2 == 1.5
 
-def  test_numpy_partition(): 
-     input_arr = np.aray()[3, 2, -1, 100 ]
-     partitioned = np.partition(input_arr, 1)
-     assert partitioned[1] == 2
+
+def partition_wrapper(arr, kth):
+    try:
+        return np.partition(arr, kth)
+    except AttributeError:
+        return np.sort(arr)
+
+
+def test_partition():
+    input_array = np.array([-10, 3, 1, 5, -7])
+    partitioned_array = partition_wrapper(input_array, 2)
+    assert partitioned_array[2] == 1
